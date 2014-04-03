62fc84c85ea1f534931514d38df9ffdda517a77e
Daniel Rojas <danielrojas121@gmail.com>
2014-04-03 03:11:04 -0400

    Added search functionality to home.html and created a placeholder results.html page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config["DEBUG"] = True  # Only include this while you are testing your app
@@ -6,6 +6,13 @@ app.config["DEBUG"] = True  # Only include this while you are testing your app
 @app.route("/")
 def home():
 	return render_template("home.html")
+
+@app.route("/search", methods=["GET","POST"])
+def search():
+	if request.method == "POST":
+		return render_template("results.html")
+	else:
+		return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
