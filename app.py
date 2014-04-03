19aefc2685658f2e137a70f97d4ba44a0ea78793
Daniel Rojas <danielrojas121@gmail.com>
2014-04-03 01:40:29 -0400

    Added a templates directory with example home.html and modified app.py with a dynamic home route.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True  # Only include this while you are testing your app
 
 @app.route("/")
-def hello():
-	return "Hello World! Helloooooooo!"
+def home():
+	return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
