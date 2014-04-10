5e3c7309d16c1fa5cf31d9a9ae2ad1cd53d872b2
Daniel Rojas <danielrojas121@gmail.com>
2014-04-09 22:30:00 -0400

    Added user search form grab.

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True  # Only include this while you are testing your app
+
+json_data = open('phones.json').read()
+data = json.loads(json_data)
+json_data.close()
 
 @app.route("/")
 def home():
@@ -10,7 +15,7 @@ def home():
 @app.route("/search", methods=["GET","POST"])
 def search():
 	if request.method == "POST":
-		return render_template("results.html")
+		return render_template("results.html", phone=request.form['user_search'])
 	else:
 		return render_template("home.html")
 
