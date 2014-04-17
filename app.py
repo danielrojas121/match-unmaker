1ec02e5bdec33c13f254a453118083a2b34006de
Frank Cabada <frankcabada@yahoo.com>
2014-04-16 23:46:55 -0400

    Created a submit button and added a question.

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def questionaire():
 	if request.method == "GET":
 		return render_template("questionaire.html")
 	else:
-		return render_template("home.html")
+		return render_template("results.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
